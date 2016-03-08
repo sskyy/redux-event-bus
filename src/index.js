8d6bad24d0cd34d49d6c53d70dc018d9a98e9400
加缪 <jiamiu.hzy@alibaba-inc.com>
2016-03-08 14:00:45 +0800

    0.0.3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import { listen, name,  fromReduxAction } from './util'
 import createEnhancer from './createEnhancer'
 import monitor from './monitor'
-//import * as util from './util'
 
 export {
   listen, name, monitor, fromReduxAction,
